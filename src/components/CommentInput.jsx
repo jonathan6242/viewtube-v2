d84212e120380c2a09bf169fc8b2eedfb00bfbc0
Jonathan Wong <jonathanfwong@gmail.com>
2023-08-17 22:48:11 +1000

    fixed buttons on mobile

--- src/components/CommentInput.jsx
+++ src/components/CommentInput.jsx
@@ -1,12 +1,27 @@
-import { addDoc, arrayUnion, collection, doc, updateDoc } from "firebase/firestore";
+import {
+  addDoc,
+  arrayUnion,
+  collection,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
 import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import { db } from "../firebase";
 import useAuthUser from "../hooks/useAuthUser";
 
-function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile, 
-  text, setText, comment, replies, videoUID }) {
+function CommentInput({
+  reply,
+  setReplyInputOpen,
+  mobile,
+  inputRefMobile,
+  text,
+  setText,
+  comment,
+  replies,
+  videoUID,
+}) {
   const [active, setActive] = useState(false);
   const [focus, setFocus] = useState(false);
   const inputRef = useRef();
@@ -14,21 +29,21 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
   const { id } = useParams();
 
   useEffect(() => {
-    if(reply) {
-      setActive(true)
-      setFocus(true)
-      inputRef?.current?.focus()
+    if (reply) {
+      setActive(true);
+      setFocus(true);
+      inputRef?.current?.focus();
     }
-  }, [])
+  }, []);
 
   const onSubmitComment = async (e) => {
     e.preventDefault();
-    if(!user) {
-      toast.info('Sign in to comment.', { theme: 'colored' });
+    if (!user) {
+      toast.info("Sign in to comment.", { theme: "colored" });
       return;
     }
-    if(text.trim() === '') return 
-    setText('');
+    if (text.trim() === "") return;
+    setText("");
     setActive(false);
 
     await updateDoc(doc(db, "users", videoUID), {
@@ -39,9 +54,9 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
         photoURL: user?.photoURL,
         type: "comment",
         to: `/video/${id}`,
-        dateCreated: Date.now()
-      })
-    })
+        dateCreated: Date.now(),
+      }),
+    });
 
     await addDoc(collection(db, "videos", id, "comments"), {
       likes: [],
@@ -51,39 +66,41 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
       displayName: user?.displayName,
       photoURL: user?.photoURL,
       uid: user?.uid,
-    })
-  }
+    });
+  };
 
   const onSubmitReply = async (e) => {
     e.preventDefault();
-    if(!user) {
-      toast.info('Sign in to reply.', { theme: 'colored' });
+    if (!user) {
+      toast.info("Sign in to reply.", { theme: "colored" });
       return;
     }
-    if(text.trim() === '') return 
+    if (text.trim() === "") return;
     let mentions;
 
-    for(let reply of replies) {
+    for (let reply of replies) {
       const name = reply?.displayName;
-      if(text.indexOf(name) === 1 && 
-      (text[name.length + 1] === ' ' || !text[name.length + 1])) {
+      if (
+        text.indexOf(name) === 1 &&
+        (text[name.length + 1] === " " || !text[name.length + 1])
+      ) {
         mentions = {
           uid: reply?.uid,
-          displayName: name
-        }
+          displayName: name,
+        };
         break;
       }
     }
-    
-    setText('');
+
+    setText("");
     setReplyInputOpen(false);
 
     // If reply does not mention anyone, notify everyone in the replies + the comment author
-    if(!mentions) {
+    if (!mentions) {
       let added = [];
-      for(let reply of replies) {
-        if(!added.includes(reply?.uid)) {
-          added.push(reply?.uid)
+      for (let reply of replies) {
+        if (!added.includes(reply?.uid)) {
+          added.push(reply?.uid);
           await updateDoc(doc(db, "users", reply?.uid), {
             notifications: arrayUnion({
               content: text,
@@ -92,12 +109,12 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
               photoURL: user?.photoURL,
               type: "reply",
               to: `/video/${id}`,
-              dateCreated: Date.now()
-            })
-          })
+              dateCreated: Date.now(),
+            }),
+          });
         }
       }
-      if(!added.includes(comment?.uid)) {
+      if (!added.includes(comment?.uid)) {
         await updateDoc(doc(db, "users", comment?.uid), {
           notifications: arrayUnion({
             content: text,
@@ -106,14 +123,14 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
             photoURL: user?.photoURL,
             type: "reply",
             to: `/video/${id}`,
-            dateCreated: Date.now()
-          })
-        })
+            dateCreated: Date.now(),
+          }),
+        });
       }
     }
 
     // If reply mentions someone, notify them
-    if(mentions) {
+    if (mentions) {
       await updateDoc(doc(db, "users", mentions.uid), {
         notifications: arrayUnion({
           content: text,
@@ -122,9 +139,9 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
           photoURL: user?.photoURL,
           type: "reply",
           to: `/video/${id}`,
-          dateCreated: Date.now()
-        })
-      })
+          dateCreated: Date.now(),
+        }),
+      });
     }
 
     const replyObject = {
@@ -134,209 +151,201 @@ function CommentInput({ reply, setReplyInputOpen, mobile, inputRefMobile,
       comment: text,
       displayName: user.displayName,
       photoURL: user.photoURL,
-      uid: user.uid
-    }
-    if(mentions) {
+      uid: user.uid,
+    };
+    if (mentions) {
       replyObject.mentions = mentions;
     }
 
     await addDoc(
       collection(db, "videos", id, "comments", comment.id, "replies"),
       replyObject
-    )
-
-
-  }
+    );
+  };
 
   // Desktop
-  if(!mobile) {
+  if (!mobile) {
     return (
       <>
-        {
-          !reply ? (
-            <div className="flex items-start mb-8 space-x-4">
-              {
-                user ? (
-                  <div 
-                    className="mt-1 w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat flex-shrink-0 md:w-12 md:h-12 md:mt-0"
-                    style={{
-                      backgroundImage: `url(${user?.photoURL})`
-                    }}
-                  >
-                  </div>
-                ) : (
-                  <div
-                    className="mt-1 rounded-full material-symbols-outlined fill-1 text-[40px]
+        {!reply ? (
+          <div className="flex items-start mb-8 space-x-4">
+            {user ? (
+              <div
+                className="mt-1 w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat flex-shrink-0 md:w-12 md:h-12 md:mt-0"
+                style={{
+                  backgroundImage: `url(${user?.photoURL})`,
+                }}
+              ></div>
+            ) : (
+              <div
+                className="mt-1 rounded-full material-symbols-outlined fill-1 text-[40px]
                     flex-shrink-0 md:mt-0 md:text-[48px] flex justify-center items-center"
-                  >
-                    account_circle
-                  </div>
-                )
-              }
-
-              <form
-                className="flex-1 flex flex-col"
-                onSubmit={onSubmitComment}
               >
-                <div className="flex-1 flex relative justify-center">
-                  <input 
-                    type="text"
-                    className={`flex-1 self-stretch bg-transparent text-base placeholder:text-secondary outline-none transition duration-200 pb-1 border-b`}
-                    placeholder={`Add a comment...`}
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    onFocus={() => {
-                      setFocus(true)
-                      setActive(true)
-                    }}
-                    onBlur={() => {
-                      setFocus(false)
-                    }}
-                  />
-                  <div className={`absolute 
-                  ${focus ? 'h-[2px] -bottom-[1px]' : 'h-[1px] -bottom-0 bg-gray-500 dark:bg-gray-400'} 
-                  ${active ? 'w-full' : 'w-0'} duration-200
-                  bg-black dark:bg-white`}></div>
-                </div>
-                {
-                  active && (
-                    <div className="mt-2 self-end flex space-x-2 text-sm font-medium">
-                      <button
-                        type='button'
-                        className="py-[10px] px-4 opacity-90 uppercase "
-                        onClick={() => {
-                          setActive(false)
-                          setText('')
-                        }}
-                      >
-                        Cancel
-                      </button>
-                      
-                      <button
-                        className={`py-[10px] px-5 rounded uppercase ${text.length ? 'bg-blue-500 text-white' : 'bg-gray-100 dark:bg-dark2 text-secondary'}`}
-                      >
-                        <span>{reply ? 'Reply' : 'Comment'}</span> 
-                      </button>
-                    </div>
-                  )
-                }
-        
-              </form>
-            </div>
-          ) : (
-            <div className="flex items-start space-x-4 mt-1">
-              {
-                user ? (
-                  <div 
-                    className="w-8 h-8 rounded-full bg-cover bg-center bg-no-repeat"
-                    style={{
-                      backgroundImage: `url(${user?.photoURL})`
-                    }}
-                  ></div>
-                ) : (
-                  <div
-                    className="material-symbols-outlined fill-1 text-[32px] flex justify-center items-center"
-                  >
-                    account_circle
-                  </div>
-                )
-              }
+                account_circle
+              </div>
+            )}
 
-
-              <form 
-                className="flex-1 flex flex-col justify-start"
-                onSubmit={onSubmitReply}
-              >
-                <div className="flex-1 flex relative justify-center">
-                  <input 
-                    type="text"
-                    className="flex-1 self-stretch bg-transparent text-base placeholder:text-secondary outline-none border-b pb-1 transition duration-200"
-                    placeholder="Add a reply..."
-                    ref={inputRef}
-                    value={text}
-                    onChange={(e) => setText(e.target.value)}
-                    onFocus={() => {
-                      setFocus(true)
-                      setActive(true)
-                    }}
-                    onBlur={() => {
-                      setFocus(false)
-                    }}
-                  />
-                  <div className={`absolute 
-                  ${focus ? 'h-[2px] -bottom-[1px]' : 'h-[1px] -bottom-0 bg-gray-500 dark:bg-gray-400'} 
-                  ${active ? 'w-full' : 'w-0'} duration-200
-                bg-black dark:bg-white`}></div>
-                </div>
+            <form className="flex-1 flex flex-col" onSubmit={onSubmitComment}>
+              <div className="flex-1 flex relative justify-center">
+                <input
+                  type="text"
+                  className={`flex-1 self-stretch bg-transparent text-base placeholder:text-secondary outline-none transition duration-200 pb-1 border-b`}
+                  placeholder={`Add a comment...`}
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  onFocus={() => {
+                    setFocus(true);
+                    setActive(true);
+                  }}
+                  onBlur={() => {
+                    setFocus(false);
+                  }}
+                />
+                <div
+                  className={`absolute 
+                  ${
+                    focus
+                      ? "h-[2px] -bottom-[1px]"
+                      : "h-[1px] -bottom-0 bg-gray-500 dark:bg-gray-400"
+                  } 
+                  ${active ? "w-full" : "w-0"} duration-200
+                  bg-black dark:bg-white`}
+                ></div>
+              </div>
+              {active && (
                 <div className="mt-2 self-end flex space-x-2 text-sm font-medium">
                   <button
-                    type='button'
+                    type="button"
                     className="py-[10px] px-4 opacity-90 uppercase "
                     onClick={() => {
-                      setReplyInputOpen(false)
-                      setText('');
+                      setActive(false);
+                      setText("");
                     }}
                   >
                     Cancel
                   </button>
-                  <button 
-                    className={`py-[10px] px-5 rounded uppercase ${text.length ? 'bg-blue-500 text-white' : 'bg-gray-100 dark:bg-dark2 text-secondary'}`}
+
+                  <button
+                    className={`py-[10px] px-5 rounded uppercase ${
+                      text.length
+                        ? "bg-blue-500 text-white"
+                        : "bg-gray-100 dark:bg-dark2 text-secondary"
+                    }`}
                   >
-                    Reply
+                    <span>{reply ? "Reply" : "Comment"}</span>
                   </button>
                 </div>
-              </form>
-            </div>
-          )
-        }
-      </>
-    )
-  }
+              )}
+            </form>
+          </div>
+        ) : (
+          <div className="flex items-start space-x-4 mt-1">
+            {user ? (
+              <div
+                className="w-8 h-8 rounded-full bg-cover bg-center bg-no-repeat"
+                style={{
+                  backgroundImage: `url(${user?.photoURL})`,
+                }}
+              ></div>
+            ) : (
+              <div className="material-symbols-outlined fill-1 text-[32px] flex justify-center items-center">
+                account_circle
+              </div>
+            )}
 
+            <form
+              className="flex-1 flex flex-col justify-start"
+              onSubmit={onSubmitReply}
+            >
+              <div className="flex-1 flex relative justify-center">
+                <input
+                  type="text"
+                  className="flex-1 self-stretch bg-transparent text-base placeholder:text-secondary outline-none border-b pb-1 transition duration-200"
+                  placeholder="Add a reply..."
+                  ref={inputRef}
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  onFocus={() => {
+                    setFocus(true);
+                    setActive(true);
+                  }}
+                  onBlur={() => {
+                    setFocus(false);
+                  }}
+                />
+                <div
+                  className={`absolute 
+                  ${
+                    focus
+                      ? "h-[2px] -bottom-[1px]"
+                      : "h-[1px] -bottom-0 bg-gray-500 dark:bg-gray-400"
+                  } 
+                  ${active ? "w-full" : "w-0"} duration-200
+                bg-black dark:bg-white`}
+                ></div>
+              </div>
+              <div className="mt-2 self-end flex space-x-2 text-sm font-medium">
+                <button
+                  type="button"
+                  className="py-[10px] px-4 opacity-90 uppercase "
+                  onClick={() => {
+                    setReplyInputOpen(false);
+                    setText("");
+                  }}
+                >
+                  Cancel
+                </button>
+                <button
+                  className={`py-[10px] px-5 rounded uppercase ${
+                    text.length
+                      ? "bg-blue-500 text-white"
+                      : "bg-gray-100 dark:bg-dark2 text-secondary"
+                  }`}
+                >
+                  Reply
+                </button>
+              </div>
+            </form>
+          </div>
+        )}
+      </>
+    );
+  }
 
   // Mobile
   return (
     <form onSubmit={reply ? onSubmitReply : onSubmitComment}>
       <div className="flex mb-0 space-x-2">
-        {
-          user ? (
-            <div 
-              className="w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat flex-shrink-0
+        {user ? (
+          <div
+            className="w-10 h-10 rounded-full bg-cover bg-center bg-no-repeat flex-shrink-0
               md:w-12 md:h-12"
-              style={{
-                backgroundImage: `url(${user?.photoURL})`
-              }}
-            ></div>
-          ) : (
-            <div
-              className="material-symbols-outlined fill-1 text-[40px] flex-shrink-0 md:text-[48px] flex justify-center items-center"
-            >
-              account_circle
-            </div>
-          )
-        }
-        <div className="flex-1">
-          <input 
+            style={{
+              backgroundImage: `url(${user?.photoURL})`,
+            }}
+          ></div>
+        ) : (
+          <div className="material-symbols-outlined fill-1 text-[40px] flex-shrink-0 md:text-[48px] flex justify-center items-center">
+            account_circle
+          </div>
+        )}
+        <div className="flex-1 relative flex items-center">
+          <input
             type="text"
             className={`w-full placeholder:text-secondary outline-none transition duration-200 pl-2 py-2 pr-0 border border-transparent bg-gray-100 dark:bg-dark2 rounded`}
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder={`Add a ${reply ? 'reply' : 'comment'}...`}
+            placeholder={`Add a ${reply ? "reply" : "comment"}...`}
             ref={inputRefMobile}
           />
+          <button className="absolute inset-y-0 right-0 w-10 flex items-center">
+            <span className="material-symbols-outlined fill-1 text-2xl absolute right-3">
+              send
+            </span>
+          </button>
         </div>
       </div>
-      <div className="mt-2 flex justify-end space-x-2 text-sm font-medium">
-        <button
-          className="py-[10px] px-5 rounded bg-gray-100 dark:bg-dark2 text-secondary uppercase space-x-2 flex items-center"
-        >
-          <span className="material-symbols-outlined fill-1 text-xl">
-            send
-          </span>
-          <span>{reply ? 'Reply' : 'Comment'}</span> 
-        </button>
-      </div>
     </form>
-  )
+  );
 }
-export default CommentInput
+export default CommentInput;
